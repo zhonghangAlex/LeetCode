aa5bc9a78c96e15c56b25b9691fa9f56a54e62b6
yinguohui <yinguohui@bytedance.com>
2019-03-13 00:47:31 +0800

    修改字符串&数组&正则三个章节的代码；修复了LeetCode无法通过的case，LeetCode不合理的地方也在源码中做了说明

--- test/array/lesson3.test.js
+++ test/array/lesson3.test.js
@@ -6,3 +6,6 @@ test('flower:[1,0,0,0,1],1', () => {
 test('flower:[1,0,0,0,1],2', () => {
   expect(flower([1, 0, 0, 0, 1], 2)).toBe(false)
 })
+test('flower:[1, 0, 0, 0, 1, 0, 0], 2', () => {
+  expect(flower([1, 0, 0, 0, 1, 0, 0], 2)).toBe(true)
+})
--- test/regexp/lesson2.test.js
+++ test/regexp/lesson2.test.js
@@ -1,11 +1,9 @@
 import regModeStr from '../../code/regexp/lesson2'
 
 test('regModeStr', () => {
-  expect(regModeStr('aaa', 'caa*')).toBe(false)
-})
-test('regModeStr:2', () => {
-  expect(regModeStr('aaabc', 'a*bc')).toBe(true)
-})
-test('regModeStr:3', () => {
-  expect(regModeStr('aaabc', 'a*.*')).toBe(true)
+  expect(regModeStr('aa', 'a')).toBe(false)
+  expect(regModeStr('aa', 'a*')).toBe(true)
+  expect(regModeStr('ab', '.*')).toBe(true)
+  expect(regModeStr('aab', 'c*a*b')).toBe(true)
+  expect(regModeStr('mississippi', 'mis*is*p*.')).toBe(false)
 })
