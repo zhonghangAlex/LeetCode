877622918eed3c1a1eff22460e0b104f6dc7b6fa
yinguohui <yinguohui@bytedance.com>
2019-03-23 22:15:15 +0800

    调整动态规划第二道题目的参数

--- code/dp/lesson2.js
+++ code/dp/lesson2.js
@@ -1,10 +1,6 @@
-export default (src, dst, k) => {
-  let fights = [
-    [0, 1, 100],
-    [1, 2, 100],
-    [0, 2, 500]
-  ]
-  let cheap = (src, dst, k) => {
+export default (fights, src, dst, k) => {
+  // 将fights作为参数和LeetCode一致
+  let cheap = (fights, src, dst, k) => {
     let prev = fights.filter(item => item[1] === dst)
     let min = Math.min.apply(null, prev.map(item => {
       if (item[0] === src && k > -1) {
@@ -12,10 +8,12 @@ export default (src, dst, k) => {
       } else if (k === 0 && item[0] !== src) {
         return Number.MAX_SAFE_INTEGER
       } else {
-        return item[2] + cheap(src, item[0], k - 1)
+        return item[2] + cheap(fights, src, item[0], k - 1)
       }
     }))
     return min
   }
-  return cheap(src, dst, k)
+  // 增加返回值是不是Number.MAX_SAFE_INTEGER，如果是返回-1
+  let min = cheap(fights, src, dst, k)
+  return min >= Number.MAX_SAFE_INTEGER ? -1 : min
 }
